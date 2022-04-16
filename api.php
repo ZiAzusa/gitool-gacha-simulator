3a2216f6bf0d4c90a42d52128ef9d3729107e558
0803QwQ <64982342+0803QwQ@users.noreply.github.com>
2022-04-16 12:33:59 +0800

    Add files via upload

---
+++ api.php
@@ -0,0 +1,103 @@
+<?php
+$items = file_get_contents("items.json");
+$itemsArr = json_decode($items, true);
+if ($_POST['askup'] != null){
+    $arrayName = "now-".$_POST['poolname'];
+    $r5up = $itemsArr['r5'][$arrayName];
+    if (count($r5up) != 10 || count($r5up) != 13){
+        foreach ($r5up as $r5k => $r5v) $r5upText .= $r5v[1]." ";
+    }else{
+        $r5upText = $r5up[1];
+    };
+    $r4up = $itemsArr['r4'][$arrayName];
+    if (count($r4up) != 10 || count($r4up) != 13){
+        foreach ($r4up as $r4k => $r4v) $r4upText .= $r4v[1]." ";
+    }else{
+        $r4upText = $r4up[1];
+    };
+    print_r(json_encode(['upr5' => $r5upText, 'upr4' => $r4upText]));
+    exit;
+};
+$info = $_POST['gachalog'][$_POST['poolname']];
+switch ($_POST['poolname']){
+    case 'arm':
+        $flootnum = 80;
+        $pluscell = 99400;
+        break;
+    default:
+        $flootnum = 90;
+        $pluscell = 49700;
+        break;
+};
+$info['c'] += 1;
+$count = $info['c'] - $info['r5c'];
+$r5per = 6000;
+$r4per = 51000;
+if ($count >= 70) $r5per += ($count - 70) * $pluscell;
+$seed = mt_rand(1, 1000000);
+if ($seed <= $r5per || $count == $flootnum){
+    switch ($_POST['poolname']) {
+        case 'chr':
+            $chrseed = mt_rand(1, 1000000);
+            foreach ($itemsArr['r5']['always-chr'] as $ar5v) $ar5[] = $ar5v[1];
+            if (($chrseed <= 500000 || in_array($info['lastr5'], $ar5v)) && $itemsArr['r5']['now-chr'] != null){
+                if (is_array($itemsArr['r5']['now-chr'])){
+                    $result = $itemsArr['r5']['now-chr'][array_rand($itemsArr['r5']['now-chr'])];
+                }else{
+                    $result = $itemsArr['r5']['now-chr'];
+                };
+            }else{
+                $result = $itemsArr['r5']['always-chr'][array_rand($itemsArr['r5']['always-chr'])];
+            };
+            break;
+        case 'arm':
+            $armseed = mt_rand(1, 1000000);
+            foreach ($itemsArr['r5']['always-arm'] as $ar5v) $ar5[] = $ar5v[1];
+            if (($armseed <= 500000 || in_array($info['lastr5'], $ar5v)) && $itemsArr['r5']['now-arm'] != null){
+                if (is_array($itemsArr['r5']['now-arm'])){
+                    $result = $itemsArr['r5']['now-arm'][array_rand($itemsArr['r5']['now-arm'])];
+                }else{
+                    $result = $itemsArr['r5']['now-arm'];
+                };
+            }else{
+                $result = $itemsArr['r5']['always-arm'][array_rand($itemsArr['r5']['always-arm'])];
+            };
+            break;
+        default:
+            $fullR5Arr = array_merge($itemsArr['r5']['always-chr'], $itemsArr['r5']['always-arm']);
+            $result = $fullR5Arr[array_rand($fullR5Arr)];
+            break;
+    };
+}elseif (($seed > $r5per && $seed <= ($r5per + $r4per)) || ($info['c'] % 10) == 0){
+    switch ($_POST['poolname']) {
+        case 'chr':
+            $chrseed = mt_rand(1, 1000000);
+            if ($chrseed <= 500000 && $itemsArr['r4']['now-chr'] != null){
+                $result = $itemsArr['r4']['now-chr'][array_rand($itemsArr['r4']['now-chr'])];
+            }else{
+                $result = $itemsArr['r4']['always'][array_rand($itemsArr['r4']['always'])];
+            };
+            break;
+        case 'arm':
+            $armseed = mt_rand(1, 1000000);
+            if ($armseed <= 500000 || $itemsArr['r4']['now-arm'] != null){
+                $result = $itemsArr['r4']['now-arm'][array_rand($itemsArr['r4']['now-arm'])];
+            }else{
+                $result = $itemsArr['r4']['always'][array_rand($itemsArr['r4']['always'])];
+            };
+            break;
+        default:
+            $result = $itemsArr['r4']['always'][array_rand($itemsArr['r4']['always'])];
+            break;
+    };
+}else{
+    $result = $itemsArr['r3']['always'][array_rand($itemsArr['r3']['always'])];
+};
+if (count($result) == 13){
+    $rank = $result[2];
+}elseif (count($result) == 10){
+    $rank = $result[3];
+};
+print_r(json_encode(['rank' => $rank, 'img' => "<div><img src=\"".$result[0]."\" style='hight:40px;width:40px' align='absmiddle'/>",'name' => $result[1]]));
+exit;
+?>
---
+++ js/index.js
@@ -0,0 +1,249 @@
+var chr = document.getElementById("chr");
+var arm = document.getElementById("arm");
+var nov = document.getElementById("nov");
+function start() {
+    $().setValue(1);
+}
+function pool(id) {
+    pool1 = document.getElementById("pool1");
+    pool2 = document.getElementById("pool2");
+    pool3 = document.getElementById("pool3");
+    if (chr.style.display == "block") {
+        all = document.getElementById("Call-table");
+        r5 = document.getElementById("Cr5-table");
+        r4 = document.getElementById("Cr4-table");
+    } else if (arm.style.display == "block") {
+        all = document.getElementById("Aall-table");
+        r5 = document.getElementById("Ar5-table");
+        r4 = document.getElementById("Ar4-table");
+    } else if (nov.style.display == "block") {
+        all = document.getElementById("Nall-table");
+        r5 = document.getElementById("Nr5-table");
+        r4 = document.getElementById("Nr4-table");
+    }
+    if (all.style.display == "block") {
+        showid = 1;
+    } else if (r5.style.display == "block") {
+        showid = 2;
+    } else if (r4.style.display == "block") {
+        showid = 3;
+    }
+    if (id == 1) {
+        pool1.style = "border-width:0px;background:#FFB31E;color:#FFFFFF";
+        pool2.style = "border-width:2px;background:rgba(255,255,255,0.8);color:#FFB31E";
+        pool3.style = "border-width:2px;background:rgba(255,255,255,0.8);color:#FFB31E";
+        chr.style.display = "block";
+        arm.style.display = "none";
+        nov.style.display = "none";
+    } else if (id == 2) {
+        pool2.style = "border-width:0px;background:#FFB31E;color:#FFFFFF";
+        pool3.style = "border-width:2px;background:rgba(255,255,255,0.8);color:#FFB31E";
+        pool1.style = "border-width:2px;background:rgba(255,255,255,0.8);color:#FFB31E";
+        arm.style.display = "block";
+        nov.style.display = "none";
+        chr.style.display = "none";
+    } else if (id == 3) {
+        pool3.style = "border-width:0px;background:#FFB31E;color:#FFFFFF";
+        pool1.style = "border-width:2px;background:rgba(255,255,255,0.8);color:#FFB31E";
+        pool2.style = "border-width:2px;background:rgba(255,255,255,0.8);color:#FFB31E";
+        nov.style.display = "block";
+        chr.style.display = "none";
+        arm.style.display = "none";
+    }
+    show(showid);
+    $().setValue(id);
+}
+function show(id) {
+    show1 = document.getElementById("show1");
+    show2 = document.getElementById("show2");
+    show3 = document.getElementById("show3");
+    if (chr.style.display == "block") {
+        all = document.getElementById("Call-table");
+        r5 = document.getElementById("Cr5-table");
+        r4 = document.getElementById("Cr4-table");
+    } else if (arm.style.display == "block") {
+        all = document.getElementById("Aall-table");
+        r5 = document.getElementById("Ar5-table");
+        r4 = document.getElementById("Ar4-table");
+    } else if (nov.style.display == "block") {
+        all = document.getElementById("Nall-table");
+        r5 = document.getElementById("Nr5-table");
+        r4 = document.getElementById("Nr4-table");
+    }
+    if (id == 1) {
+        show1.style = "border-width:0px;background:#32EB00;color:#FFFFFF";
+        show2.style = "border-width:2px;background:rgba(255,255,255,0.8);color:#32EB00";
+        show3.style = "border-width:2px;background:rgba(255,255,255,0.8);color:#32EB00";
+        all.style.display = "block";
+        r5.style.display = "none";
+        r4.style.display = "none";
+    } else if (id == 2) {
+        show2.style = "border-width:0px;background:#32EB00;color:#FFFFFF";
+        show3.style = "border-width:2px;background:rgba(255,255,255,0.8);color:#32EB00";
+        show1.style = "border-width:2px;background:rgba(255,255,255,0.8);color:#32EB00";
+        r5.style.display = "block";
+        r4.style.display = "none";
+        all.style.display = "none";
+    } else if (id == 3) {
+        show3.style = "border-width:0px;background:#32EB00;color:#FFFFFF";
+        show1.style = "border-width:2px;background:rgba(255,255,255,0.8);color:#32EB00";
+        show2.style = "border-width:2px;background:rgba(255,255,255,0.8);color:#32EB00";
+        r4.style.display = "block";
+        all.style.display = "none";
+        r5.style.display = "none";
+    }
+}
+$(function() {
+    var CR5num = 0;
+    var CR4num = 0;
+    var AR5num = 0;
+    var AR4num = 0;
+    var NR5num = 0;
+    var NR4num = 0;
+    var gachalog = {};
+    gachalog['chr'] = {};
+    gachalog['arm'] = {};
+    gachalog['nov'] = {};
+    for (var key in gachalog) {
+        gachalog[key]['c'] = 0;
+        gachalog[key]['r5c'] = 0;
+    }
+    $.fn.setValue = function(id) {
+        if (id == 1) {
+            poolname = 'chr';
+            flootnum = 90;
+            R5num = CR5num;
+            R4num = CR4num;
+        } else if (id == 2) {
+            poolname = 'arm';
+            flootnum = 80;
+            R5num = AR5num;
+            R4num = AR4num;
+        } else if (id == 3) {
+            poolname = 'nov';
+            flootnum = 90;
+            R5num = NR5num;
+            R4num = NR4num;
+        }
+        $("[name='times']").html(gachalog[poolname]['c']);
+        $("[name='spend']").html(gachalog[poolname]['c'] * 160);
+        $("[name='r3num']").html(gachalog[poolname]['c'] - R5num - R4num);
+        $("[name='r3per']").html(((gachalog[poolname]['c'] - R5num - R4num) / gachalog[poolname]['c'] * 100).toFixed(2));
+        $("[name='r4num']").html(R4num);
+        $("[name='r4per']").html((R4num / gachalog[poolname]['c'] * 100).toFixed(2));
+        $("[name='r5num']").html(R5num);
+        $("[name='r5per']").html((R5num / gachalog[poolname]['c'] * 100).toFixed(2));
+        $("[name='floot']").html(flootnum - (gachalog[poolname]['c'] - gachalog[poolname]['r5c']));
+        if (id != 3) {
+            var postdata = {};
+            postdata['askup'] = true;
+            postdata['poolname'] = poolname;
+            $("[name='up']").css('display', 'block');
+            $.ajax({
+                type: "post",
+                url: "api.php",
+                data: postdata,
+                dataType: 'json',
+                success: function(rs) {
+                    if (rs.upr5 == null) {
+                        rs.upr5 = "无UP";
+                    }
+                    if (rs.upr4 == null) {
+                        rs.upr4 = "无UP";
+                    }
+                    $("[name='upr5']").html(rs.upr5);
+                    $("[name='upr4']").html(rs.upr4);
+                }
+            });
+        } else {
+            $("[name='up']").css('display', 'none');
+        }
+    }
+    async function gacha(poolname, times) {
+        var postdata = {};
+        postdata['gachalog'] = gachalog;
+        postdata['poolname'] = poolname;
+        $('#loading').css('display', 'block');
+        i = 1;
+        while (i <= times) {
+            await $.ajax({
+                type: "post",
+                url: "api.php",
+                data: postdata,
+                dataType: 'json',
+                success: function(rs) {
+                    if ($('#chr').css("display") == "block") {
+                        AtbMain = "#CAtbMain";
+                        R5tbMain = "#CR5tbMain";
+                        R4tbMain = "#CR4tbMain";
+                        id = 1;
+                        gachalog['chr']['c'] = gachalog['chr']['c'] + 1;
+                        num = gachalog['chr']['c'];
+                    } else if ($('#arm').css("display") == "block") {
+                        AtbMain = "#AAtbMain";
+                        R5tbMain = "#AR5tbMain";
+                        R4tbMain = "#AR4tbMain";
+                        id = 2;
+                        gachalog['arm']['c'] = gachalog['arm']['c'] + 1;
+                        num = gachalog['arm']['c'];
+                    } else if ($('#nov').css("display") == "block") {
+                        AtbMain = "#NAtbMain";
+                        R5tbMain = "#NR5tbMain";
+                        R4tbMain = "#NR4tbMain";
+                        id = 3;
+                        gachalog['nov']['c'] = gachalog['nov']['c'] + 1;
+                        num = gachalog['nov']['c'];
+                    }
+                    $(AtbMain).after("<tr><td>" + num + "</td><td>" + rs.rank + "</td><td style='text-align:left'>" + rs.img + " " + rs.name + "</td></tr>");
+                    if (rs.rank == "5星") {
+                        $(R5tbMain).after("<tr><td>" + num + "</td><td>" + rs.rank + "</td><td style='text-align:left'>" + rs.img + " " + rs.name + "</td></tr>");
+                        if ($('#chr').css("display") == "block") {
+                            gachalog['chr']['r5c'] = gachalog['chr']['c'];
+                            gachalog['chr']['lastr5'] = rs.name;
+                            CR5num = CR5num + 1;
+                        } else if ($('#arm').css("display") == "block") {
+                            gachalog['arm']['r5c'] = gachalog['arm']['c'];
+                            gachalog['arm']['lastr5'] = rs.name;
+                            AR5num = AR5num + 1;
+                        } else if ($('#nov').css("display") == "block") {
+                            gachalog['nov']['r5c'] = gachalog['nov']['c'];
+                            NR5num = NR5num + 1;
+                        }
+                    } else if (rs.rank == "4星") {
+                        $(R4tbMain).after("<tr><td>" + num + "</td><td>" + rs.rank + "</td><td style='text-align:left'>" + rs.img + " " + rs.name + "</td></tr>");
+                        if ($('#chr').css("display") == "block") {
+                            CR4num = CR4num + 1;
+                        } else if ($('#arm').css("display") == "block") {
+                            AR4num = AR4num + 1;
+                        } else if ($('#nov').css("display") == "block") {
+                            NR4num = NR4num + 1;
+                        }
+                    }
+                    $.fn.setValue(id);
+                    i++;
+                }
+            });
+        }
+        $('#loading').css('display', 'none');
+    }
+    $("button[name=only]").on('click', function() {
+        if ($('#chr').css("display") == "block") {
+            poolname = 'chr';
+        } else if ($('#arm').css("display") == "block") {
+            poolname = 'arm';
+        } else if ($('#nov').css("display") == "block") {
+            poolname = 'nov';
+        }
+        gacha(poolname, 1);
+    });
+    $("button[name=ten]").on('click', function() {
+        if ($('#chr').css("display") == "block") {
+            poolname = 'chr';
+        } else if ($('#arm').css("display") == "block") {
+            poolname = 'arm';
+        } else if ($('#nov').css("display") == "block") {
+            poolname = 'nov';
+        }
+        gacha(poolname, 10);
+    });
+});
